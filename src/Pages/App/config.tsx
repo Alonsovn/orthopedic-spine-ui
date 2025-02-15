1407616bc53f0c389ecd024a4be7d1ab6bc4c9c6
Alonso VN <alonsonh94@gmail.com>
2025-02-15 17:28:42 -0600

    Update key and icon in sider menu items

--- src/Pages/App/config.tsx
+++ src/Pages/App/config.tsx
@@ -2,14 +2,22 @@ import AboutUs from "../AboutUs";
 import ForBusiness from "../ForBusiness";
 import Home from "../Home";
 import Services from "../Services";
-import { AlignCenterOutlined, HomeOutlined } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  AlignCenterOutlined,
+  UserOutlined,
+  ContactsOutlined,
+  ReadOutlined,
+  PictureOutlined,
+} from "@ant-design/icons";
+
 import NotFound from "../NotFound";
 import { RouteItem } from "../../Components/AppRoutes";
 import { SiderMenuItem } from "../../Components/SiderMenu";
 import Testimonies from "../Testimonies";
 import { Contact } from "../Contact";
 import { Blog } from "../Blog";
-import { Galery } from "../Gallery";
+import { Gallery } from "../Gallery";
 
 export const routes: RouteItem[] = [
   { path: "/", element: <Home /> },
@@ -19,7 +27,7 @@ export const routes: RouteItem[] = [
   { path: "for-business", element: <ForBusiness /> },
   { path: "about-us", element: <AboutUs /> },
   { path: "blog", element: <Blog /> },
-  { path: "gallery", element: <Galery /> },
+  { path: "gallery", element: <Gallery /> },
   { path: "*", element: <NotFound /> },
 ];
 
@@ -30,38 +38,38 @@ export const siderMenuitems: SiderMenuItem[] = [
     label: "Inicio",
   },
   {
-    key: "services",
+    key: "/services",
     icon: <AlignCenterOutlined />,
     label: "Servicios",
   },
   {
-    key: "testimonies",
-    icon: <AlignCenterOutlined />,
+    key: "/testimonies",
+    icon: <UserOutlined />,
     label: "Testimonios",
   },
   {
-    key: "contact",
-    icon: <AlignCenterOutlined />,
+    key: "/contact",
+    icon: <ContactsOutlined />,
     label: "Contacto",
   },
   {
-    key: "for-business",
+    key: "/for-business",
     icon: <AlignCenterOutlined />,
     label: "Empresarial",
   },
   {
-    key: "about-us",
+    key: "/about-us",
     icon: <AlignCenterOutlined />,
     label: "Acerca de",
   },
   {
-    key: "blog",
-    icon: <AlignCenterOutlined />,
+    key: "/blog",
+    icon: <ReadOutlined />,
     label: "Blog",
   },
   {
-    key: "gallery",
-    icon: <AlignCenterOutlined />,
+    key: "/gallery",
+    icon: <PictureOutlined />,
     label: "Galeria",
   },
 ];
