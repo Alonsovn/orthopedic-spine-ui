f17864e9264b1a1df0fa1d6a3c1cd0e1348679cd
Alonso VN <alonsonh94@gmail.com>
2025-02-15 17:27:53 -0600

    Adding carousel to show the clinic services

---
+++ src/Components/AppCarousel/config.tsx
@@ -0,0 +1,49 @@
+import { ServicesCard } from ".";
+import brain from "../../Assets/images/brain.png";
+import column from "../../Assets/images/column.png";
+import rehabilitation from "../../Assets/images/rehabilitation.png";
+import hands from "../../Assets/images/hands.png";
+import legs from "../../Assets/images/legs.png";
+
+export const servicesCardsItems: ServicesCard[] = [
+  {
+    id: 1,
+    title: "Cabeza",
+    description:
+      "Atención especializada para el cuidado y tratamiento de lesiones o afecciones en la cabeza.",
+    alt: "Cabeza",
+    image: brain,
+  },
+  {
+    id: 2,
+    title: "Columna",
+    description:
+      "Tratamientos para problemas de la columna vertebral, como dolores, hernias y fracturas.",
+    alt: "Columna",
+    image: column,
+  },
+  {
+    id: 3,
+    title: "Brazos",
+    description:
+      "Recuperación y tratamientos para lesiones en los brazos, muñecas y codos.",
+    alt: "Brazos",
+    image: hands,
+  },
+  {
+    id: 4,
+    title: "Piernas",
+    description:
+      "Soluciones para lesiones en las piernas, rodillas, tobillos y pies.",
+    alt: "Piernas",
+    image: legs,
+  },
+  {
+    id: 5,
+    title: "Rehabilitación",
+    description:
+      "Programas de rehabilitación para recuperar la movilidad y fuerza en cualquier parte del cuerpo.",
+    alt: "Rehabilitación",
+    image: rehabilitation,
+  },
+];
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,5 +1,18 @@
+import { Typography } from "antd";
+import { AppCarousel } from "../../Components/AppCarousel";
+import { Content } from "antd/es/layout/layout";
+import { servicesCardsItems } from "../../Components/AppCarousel/config";
+
+const { Title } = Typography;
+
 const Home = () => {
-  return <div>Inicio </div>;
+  return (
+    <Content>
+      <Title>Orthopedic Spine</Title>
+      <Title level={2}>Servicios</Title>
+      <AppCarousel servicesCards={servicesCardsItems} />
+    </Content>
+  );
 };
 
 export default Home;
