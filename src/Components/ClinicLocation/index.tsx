3307573c983fc2a3aa6aefdc21b99c5ab133e985
Alonso VN <alonsonh94@gmail.com>
2025-02-22 13:34:48 -0600

    Fix map visualization

--- src/Components/ClinicLocation/index.tsx
+++ src/Components/ClinicLocation/index.tsx
@@ -1,8 +1,8 @@
-import { Card, Col, Row, Typography } from "antd";
-import { Content } from "antd/es/layout/layout";
+import { Card, Layout, Row, Typography } from "antd";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const { Title, Text } = Typography;
+const { Content } = Layout;
 
 interface ClinicLocationProps {
   position: readonly [number, number];
@@ -10,52 +10,53 @@ interface ClinicLocationProps {
 
 export const ClinicLocation: React.FC<ClinicLocationProps> = ({ position }) => {
   return (
-    <Content style={{ padding: "20px", textAlign: "center" }}>
-      {/* <Row justify={"center"} gutter={[16, 16]}>
-        <Col xs={24} sm={20} md={16} lg={12}>
-          <Card title="Nuestra Ubicación" style={{ width: "100%" }}>
-            <MapContainer
-              center={position}
-              zoom={17}
-              scrollWheelZoom={false}
-              style={{ height: "250px", width: "100%", borderRadius: "10px" }}
-            >
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              />
-              <Marker position={position}>
-                <Popup>Clínica Ortopédica - Ubicación Exacta</Popup>
-              </Marker>
-            </MapContainer>
-          </Card>
-        </Col>
-      </Row> */}
-
-      <Row justify="center">
-        <Col xs={24} sm={20} md={16} lg={12}>
-          <Card
-            title="Contáctenos"
-            style={{ width: "100%", textAlign: "left" }}
+    <Content>
+      <Row gutter={[16, 16]}>
+        <Card style={{ width: "100%" }}>
+          <MapContainer
+            center={position || [0, 0]}
+            zoom={16}
+            scrollWheelZoom={true}
+            style={{
+              height: "250px",
+              width: "100%",
+              borderRadius: 10,
+            }}
           >
-            <Title level={4}>Orthopedic Spine </Title>
-            <Text>
-              100 mts Norte de la Municipalidad de San Rafael, Cartago
-            </Text>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={position}>
+              <Popup>
+                Orthopedic Spine -100 mts Norte de la Municipalidad de San
+                Rafael, Cartago
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </Card>
+      </Row>
 
-            <div id="clinicInfo" style={{ marginTop: "20px" }}>
-              <Title level={5}>Llámenos</Title>
-              <a href="tel:+50688888888">+506 8888-8888 </a> |{" "}
-              <a href="tel:+5069999999">+506 9999-9999 </a>
-            </div>
-            <div style={{ marginTop: "20px" }}>
-              <Title level={5}>Email</Title>
-              <a href="mailto:info.orthopedic_spine@clinics.com">
-                info.orthopedic_spine@clinics.com
-              </a>
-            </div>
-          </Card>
-        </Col>
+      <Row>
+        <Card
+          title="Contáctenos"
+          style={{ width: "100%", marginTop: 10, borderRadius: 10 }}
+        >
+          <Title level={4}>Orthopedic Spine </Title>
+          <Text>100 mts Norte de la Municipalidad de San Rafael, Cartago</Text>
+
+          <div style={{ marginTop: "20px" }}>
+            <Title level={5}>Llámenos</Title>
+            <a href="tel:+50688888888">+506 8888-8888 </a> |{" "}
+            <a href="tel:+5069999999">+506 9999-9999 </a>
+          </div>
+          <div style={{ marginTop: "20px" }}>
+            <Title level={5}>Email</Title>
+            <a href="mailto:info.orthopedic_spine@clinics.com">
+              info.orthopedic_spine@clinics.com
+            </a>
+          </div>
+        </Card>
       </Row>
     </Content>
   );
--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -1,8 +1,10 @@
-import { Col, Row, Typography } from "antd";
+import { Col, Layout, Row, Typography } from "antd";
 import { ContactForm } from "../../Components/ContactForm";
 import { ClinicLocation } from "../../Components/ClinicLocation";
+import { ScheduleAppointment } from "../../Components/ScheduleAppointment";
 
 const { Title } = Typography;
+const { Content } = Layout;
 
 export const Contact: React.FC = () => {
   const position: [number, number] = [9.868032691676943, -83.90066555786967];
@@ -17,20 +19,39 @@ export const Contact: React.FC = () => {
   };
 
   return (
-    <Row gutter={[32, 32]} justify="center" style={{ padding: "20px" }}>
-      <Col xs={24} sm={24} md={12} lg={10}>
-        <Title level={2} style={{ textAlign: "center" }}>
-          Contáctenos
-        </Title>
-        <ContactForm onFinish={onSubmitContactForm} />
+    <Row
+      gutter={{ xs: 8, sm: 16, md: 24, lg: 24 }}
+      justify="center"
+      wrap
+      style={{ padding: 10 }}
+    >
+      <Col xs={24} sm={24} md={12} lg={12}>
+        <Content
+          style={{
+            border: "1px solid #d9d9d9",
+            padding: 20,
+            borderRadius: 10,
+            minHeight: 720,
+          }}
+        >
+          <Title level={2} style={{ textAlign: "center" }}>
+            Contáctenos
+          </Title>
+          <ContactForm onFinish={onSubmitContactForm} />
+        </Content>
       </Col>
 
-      <Col xs={24} sm={24} md={12} lg={10}>
-        <Title level={2} style={{ textAlign: "center" }}>
-          ¿Cómo llegar?
-        </Title>
-        <ClinicLocation position={position} />
+      <Col xs={24} sm={24} md={12} lg={12}>
+        <Content
+          style={{ border: "1px solid #d9d9d9", padding: 20, borderRadius: 10 }}
+        >
+          <Title level={2} style={{ textAlign: "center" }}>
+            ¿Cómo llegar?
+          </Title>
+          <ClinicLocation position={position} />
+        </Content>
       </Col>
+      <ScheduleAppointment />
     </Row>
   );
 };
