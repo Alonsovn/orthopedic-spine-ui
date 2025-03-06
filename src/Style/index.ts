32ff3dc15eedd30d2c1100adae0e2f6eecc79030
Alonso VN <alonsonh94@gmail.com>
2025-03-05 21:47:16 -0600

    Centralized style

---
+++ src/Style/index.ts
@@ -0,0 +1,12 @@
+export const useLogoStyle = () => ({
+  width: 80,
+  height: 60,
+  paddingInline: 5,
+  marginTop: 10,
+  borderRadius: 30,
+});
+
+export const useDividerStyle = () => ({
+  borderColor: 'white',
+  margin: '12 0',
+});
