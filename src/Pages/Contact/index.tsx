888fbb78fa23152ff40c1585d162620a04c9cd0e
Alonso VN <alonsonh94@gmail.com>
2025-03-12 23:51:24 -0600

    Enable send emails

--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -1,8 +1,9 @@
-import { Col, Layout, Row, Typography } from 'antd';
+import { Col, Layout, message, Row, Typography } from 'antd';
 import { ContactForm } from '../../Components/ContactForm';
 import { ClinicLocation } from '../../Components/ClinicLocation';
 import { ScheduleAppointment } from '../../Components/ScheduleAppointment';
 import { useSendEmailMutation } from '../../Api/orthopedicSpineApi';
+import { useCallback } from 'react';
 
 const { Title } = Typography;
 const { Content } = Layout;
@@ -10,14 +11,27 @@ const { Content } = Layout;
 export const Contact: React.FC = () => {
   const [sendEmail] = useSendEmailMutation();
 
-  const onSubmitContactForm = (values: { name: string; email: string; message: string; confirm: boolean }) => {
-    console.log('Form values: ', values);
-    sendEmail({
-      fromUser: values.email,
-      subject: values.name,
-      message: values.message,
-    });
-  };
+  const onSubmitContactForm = useCallback(
+    async (values: { name: string; email: string; message: string; confirm: boolean }) => {
+      try {
+        const response = await sendEmail({
+          fromUser: values.email,
+          subject: values.name,
+          message: values.message,
+        }).unwrap();
+
+        if (response.error) {
+          message.error('Error al enviar el mensaje. Inténtelo de nuevo.');
+          return;
+        }
+
+        message.success('Mensaje enviado con éxito');
+      } catch (error) {
+        message.error('Error al enviar el mensaje. Inténtelo de nuevo.');
+      }
+    },
+    [sendEmail],
+  );
 
   return (
     <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 24 }} justify="center" wrap style={{ padding: 10 }}>
@@ -45,7 +59,9 @@ export const Contact: React.FC = () => {
           <ClinicLocation />
         </Content>
       </Col>
-      <ScheduleAppointment />
+      <Col xs={24} sm={24} md={12} lg={12}>
+        <ScheduleAppointment />
+      </Col>
     </Row>
   );
 };
