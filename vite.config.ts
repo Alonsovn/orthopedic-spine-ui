8a89b137ead95c22264924281f5972476e6e09b3
Alonso VN <alonsonh94@gmail.com>
2025-03-31 22:59:44 -0600

    Fix github pages release, set base: '/orthopedic-spine-ui/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'],
-  base: '/',
+  // base: '/',
+  base: '/orthopedic-spine-ui/',
 });
