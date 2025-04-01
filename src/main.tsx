d6dc93085cea4e1edfd4e3cf0730430209f646bc
Alonso VN <alonsonh94@gmail.com>
2025-03-31 21:01:31 -0600

    Revert changes for initApp

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,13 @@ import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 import { HashRouter as Router } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
-import { loadAppConfig } from './Config/configLoader';
 
-const initApp = async () => {
-  await loadAppConfig();
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <Router>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </Router>
-    </StrictMode>,
-  );
-};
-
-initApp();
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </StrictMode>,
+);
