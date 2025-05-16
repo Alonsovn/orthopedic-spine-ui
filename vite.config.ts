390476adc856c96e74bd6d2f5165fc80801d50a1
Alonso VN <alonsonh94@gmail.com>
2025-05-15 23:36:48 -0600

    Fomatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -7,16 +7,16 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    base: env.VITE_APP_BASE_URL || "/", // Set base path dynamically
+    base: env.VITE_APP_BASE_URL || '/', // Set base path dynamically
     define: {
-      "process.env": env, // Expose env variables globally
+      'process.env': env, // Expose env variables globally
     },
     build: {
-      outDir: "build",
-      assetsDir: "assets",
+      outDir: 'build',
+      assetsDir: 'assets',
       emptyOutDir: true,
-      sourcemap: mode === "development", // Enable sourcemaps in development
-      minify: mode === "production" ? "esbuild" : false, // Minify only in production
+      sourcemap: mode === 'development', // Enable sourcemaps in development
+      minify: mode === 'production' ? 'esbuild' : false, // Minify only in production
     },
   };
 });
