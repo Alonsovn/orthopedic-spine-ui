598a8070003cc11abcf10ab2837d80d83057275b
Alonso VN <alonsonh94@gmail.com>
2025-05-16 00:08:04 -0600

    Setting up CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [react()],
-    base: env.VITE_APP_BASE_URL || '/', // Set base path dynamically
-    define: {
-      'process.env': env, // Expose env variables globally
-    },
+    base: env.VITE_APP_BASE_URL || '/',
     build: {
       outDir: 'build',
       assetsDir: 'assets',
       emptyOutDir: true,
-      sourcemap: mode === 'development', // Enable sourcemaps in development
-      minify: mode === 'production' ? 'esbuild' : false, // Minify only in production
+      sourcemap: mode === 'development',
+      minify: mode === 'production' ? 'esbuild' : false,
     },
   };
 });
